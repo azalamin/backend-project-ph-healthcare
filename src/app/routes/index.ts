bc215cb3eb16da521665c55ac3c3cb7569f1c712
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-02-10 15:40:14 +0600

    feat(user): create doctor

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { SpecialtyRoutes } from "../modules/specialty/specialty.route";
+import { UserRoutes } from "../modules/user/user.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes);
+router.use("/users", UserRoutes);
 
 export const IndexRoutes = router;
