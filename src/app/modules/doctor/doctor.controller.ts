189a8f4b471093df5a8e9ed0345511c0b9f09331
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-02-15 10:44:40 +0600

    feat(doctor): get single doctor

---
+++ src/app/modules/doctor/doctor.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
+import { DoctorService } from "./doctor.service";
+
+const getAllDoctors = catchAsync(async (req: Request, res: Response) => {
+	const result = await DoctorService.getAllDoctors();
+
+	sendResponse(res, {
+		httpStatusCode: 200,
+		success: true,
+		message: "Doctors fetched successfully!",
+		data: result,
+	});
+});
+
+const getDoctorById = catchAsync(async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const result = await DoctorService.getDoctorById(id as string);
+
+	sendResponse(res, {
+		httpStatusCode: 200,
+		success: true,
+		message: "Doctor fetched successfully!",
+		data: result,
+	});
+});
+
+export const DoctorController = {
+	getAllDoctors,
+	getDoctorById,
+};
