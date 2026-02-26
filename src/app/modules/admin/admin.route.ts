662d36907cd50888788722ffc083817e2f7aab03
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-02-26 12:06:56 +0600

    admin modules done

---
+++ src/app/modules/admin/admin.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { UserRole } from "../../../generated/prisma/enums";
+import { checkAuth } from "../../middleware/checkAuth";
+import { validateRequest } from "../../middleware/validateRequest";
+import { AdminController } from "./admin.controller";
+import { updateAdminZodSchema } from "./admin.validation";
+
+const router = Router();
+
+router.get("/", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), AdminController.getAllAdmins);
+router.get("/:id", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), AdminController.getAdminById);
+router.patch(
+	"/:id",
+	checkAuth(UserRole.SUPER_ADMIN),
+	validateRequest(updateAdminZodSchema),
+	AdminController.updateAdmin,
+);
+router.delete("/:id", checkAuth(UserRole.SUPER_ADMIN), AdminController.deleteAdmin);
+
+export const AdminRoutes = router;
