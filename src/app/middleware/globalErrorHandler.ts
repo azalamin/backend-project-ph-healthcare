8bc9527c9ef746f014dcc0b5c5b29188db6924ee
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-03-13 09:13:25 +0600

    feat (specialty): upload files

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,16 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import status from "http-status";
 import z from "zod";
+import { deleteFileFromCloudinary } from "../../config/cloudinary.config";
 import { envVars } from "../../config/env";
 import AppError from "../errorHelpers/AppError";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import { IErrorResponse, IErrorSources } from "../interfaces/error.interface";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler = async (
+	err: any,
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
 	if (envVars.NODE_ENV === "development") {
 		console.log("Error from Global Error Handler", err);
+	}
+
+	if (req.file) {
+		await deleteFileFromCloudinary(req.file.path);
+	}
+
+	if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+		const imageUrls = req.files.map(file => file.path);
+		await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url)));
 	}
 
 	let errorSources: IErrorSources[] = [];
